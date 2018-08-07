08d484d195379180bccfa2cae9622e5b54146e51
Josh Long <josh@joshlong.com>
2018-08-07 14:40:40 -0700

    bootiful reactive health-checked boot app

--- src/main/java/com/microsoft/azure/samples/hellospringboot/Application.java
+++ src/main/java/com/microsoft/azure/samples/hellospringboot/Application.java
@@ -1,12 +1,31 @@
 package com.microsoft.azure.samples.hellospringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+		@Bean
+		HealthIndicator health() {
+				return () -> Health.status("I <3 Production!").build();
+		}
+
+		@Bean
+		RouterFunction<ServerResponse> routes() {
+				return route(GET("/hello"), request -> ok().syncBody("Hello, Azure!"));
+		}
+
+		public static void main(String[] args) {
+				SpringApplication.run(Application.class, args);
+		}
 }
